07c9da43e5811ec248301db3b3a6d16989c7c8ae
Phil Budne <pbudne@angwin.cs.umass.edu>
2023-12-24 00:50:23 -0500

    indexer/workers/fetcher/queue-rss.py: fix character/content handling

--- indexer/workers/fetcher/queue-rss.py
+++ indexer/workers/fetcher/queue-rss.py
@@ -16,6 +16,7 @@ import logging
 import sys
 import time
 import xml.sax
+from typing import List, Optional
 
 import requests
 
@@ -28,7 +29,7 @@ logger = logging.getLogger("queue-rss")
 
 
 class RSSHandler(xml.sax.ContentHandler):
-    def __init__(self, story_sender: StorySender):
+    def __init__(self, story_sender: Optional[StorySender]):
         self.story_sender = story_sender
         self.in_item = False
         self.link = ""
@@ -38,29 +39,38 @@ class RSSHandler(xml.sax.ContentHandler):
         if name == "item":
             self.in_item = True
         self.tag = name
+        self.content: List[str] = []
 
     def characters(self, content) -> None:  # type: ignore[no-untyped-def]
         """
         text content inside current tag
         """
         # save channel.lastBuildDate for fetch date?
-        if self.in_item:
-            if self.tag == "link":
-                self.link = html.unescape(content).strip()
-            elif self.tag == "domain":
-                self.domain = content.strip()
-            # also pubDate, title
+        self.content.append(content)
 
     def endElement(self, name):  # type: ignore[no-untyped-def]
-        if self.in_item and name == "item":
+        if not self.in_item:
+            return
+
+        content = "".join(self.content)
+        # also pubDate, title
+        if name == "link":
+            self.link = html.unescape(content).strip()
+        elif name == "domain":
+            self.domain = content.strip()
+        elif name == "item":
             if self.link and self.domain:
                 s = Story()
                 with s.rss_entry() as rss:
                     rss.link = self.link
                     rss.domain = self.domain
                     # also fetch_date, pub_date, title
-                self.story_sender.send_story(s)
-                logger.info("queued %s", self.link)
+                if self.story_sender:
+                    logger.info("queued %s", self.link)
+                    self.story_sender.send_story(s)
+                else:
+                    logger.info("parsed %s", self.link)
+
                 self.link = self.domain = ""
             else:
                 logger.warning(
@@ -72,6 +82,14 @@ class RSSHandler(xml.sax.ContentHandler):
 class RSSQueuer(StoryProducer):
     def define_options(self, ap: argparse.ArgumentParser) -> None:
         super().define_options(ap)
+
+        ap.add_argument(
+            "--dry-run",
+            "-n",
+            action="store_true",
+            default=False,
+            help="don't queue stories",
+        )
 
         # NOTE! rss-fetcher generates an RSS file for the previous UTC day
         # by about 00:30 UTC
@@ -122,7 +140,11 @@ class RSSQueuer(StoryProducer):
         parse uncompressed input stream using RSSHandler (sends Stories)
         """
 
-        sender = self.story_sender()
+        assert self.args
+        if self.args.dry_run:
+            sender = None
+        else:
+            sender = self.story_sender()
 
         # XXX handle SAXParseException here, or pass error_handler=ErrorHandler
         xml.sax.parse(input, RSSHandler(sender))
